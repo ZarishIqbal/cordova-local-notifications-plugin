f314f7f6b7d1553b8f643e5d3c5ed395aa700575
zarishiqbal <zarishiqbal.zi@gmail.com>
2023-10-12 13:40:17 +0300

    retrieve action id from intent

--- src/android/notification/receiver/AbstractClickReceiver.java
+++ src/android/notification/receiver/AbstractClickReceiver.java
@@ -21,99 +21,91 @@
 
 package de.appplant.cordova.plugin.notification.receiver;
 
-import android.app.IntentService;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-
-import de.appplant.cordova.plugin.notification.Manager;
-import de.appplant.cordova.plugin.notification.Notification;
-
 import static android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
 import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
 import static de.appplant.cordova.plugin.notification.action.Action.CLICK_ACTION_ID;
 import static de.appplant.cordova.plugin.notification.action.Action.EXTRA_ID;
 
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import de.appplant.cordova.plugin.notification.Manager;
+import de.appplant.cordova.plugin.notification.Notification;
+
 /**
  * Abstract content receiver activity for local notifications. Creates the
  * local notification and calls the event functions for further proceeding.
  */
-abstract public class AbstractClickReceiver extends IntentService {
+public abstract class AbstractClickReceiver extends IntentService {
 
-    // Holds a reference to the intent to handle.
-    private Intent intent;
+  // Holds a reference to the intent to handle.
+  private Intent intent;
 
-    public AbstractClickReceiver() {
-        super("LocalNotificationClickReceiver");
-    }
+  public AbstractClickReceiver() {
+    super("LocalNotificationClickReceiver");
+  }
 
-    /**
-     * Called when local notification was clicked to launch the main intent.
-     */
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        this.intent        = intent;
+  /**
+   * Called when local notification was clicked to launch the main intent.
+   */
+  @Override
+  protected void onHandleIntent(Intent intent) {
+    this.intent = intent;
 
-        if (intent == null)
-            return;
+    if (intent == null) return;
 
-        Bundle bundle      = intent.getExtras();
-        Context context    = getApplicationContext();
+    Bundle bundle = intent.getExtras();
+    Context context = getApplicationContext();
 
-        if (bundle == null)
-            return;
+    if (bundle == null) return;
 
-        int toastId        = bundle.getInt(Notification.EXTRA_ID);
-        Notification toast = Manager.getInstance(context).get(toastId);
+    int toastId = bundle.getInt(Notification.EXTRA_ID);
+    Notification toast = Manager.getInstance(context).get(toastId);
 
-        if (toast == null)
-            return;
+    if (toast == null) return;
 
-        onClick(toast, bundle);
-        this.intent = null;
-    }
+    onClick(toast, bundle);
+    this.intent = null;
+  }
 
-    /**
-     * Called when local notification was clicked by the user.
-     *
-     * @param notification Wrapper around the local notification.
-     * @param bundle The bundled extras.
-     */
-    abstract public void onClick (Notification notification, Bundle bundle);
+  /**
+   * Called when local notification was clicked by the user.
+   *
+   * @param notification Wrapper around the local notification.
+   * @param bundle The bundled extras.
+   */
+  public abstract void onClick(Notification notification, Bundle bundle);
 
-    /**
-     * The invoked action.
-     */
-    protected String getAction() {
-        return getIntent().getExtras().getString(EXTRA_ID, CLICK_ACTION_ID);
-    }
+  /**
+   * The invoked action.
+   */
+  protected String getAction() {
+    return getIntent().getExtras().getString(CLICK_ACTION_ID, EXTRA_ID);
+  }
 
-    /**
-     * Getter for the received intent.
-     */
-    protected Intent getIntent() {
-        return intent;
-    }
+  /**
+   * Getter for the received intent.
+   */
+  protected Intent getIntent() {
+    return intent;
+  }
 
-    /**
-     * Launch main intent from package.
-     */
-    protected void launchApp() {
-        Context context = getApplicationContext();
-        String pkgName  = context.getPackageName();
+  /**
+   * Launch main intent from package.
+   */
+  protected void launchApp() {
+    Context context = getApplicationContext();
+    String pkgName = context.getPackageName();
 
-        Intent intent = context
-                .getPackageManager()
-                .getLaunchIntentForPackage(pkgName);
+    Intent intent = context
+      .getPackageManager()
+      .getLaunchIntentForPackage(pkgName);
 
-        if (intent == null)
-            return;
+    if (intent == null) return;
 
-        intent.addFlags(
-              FLAG_ACTIVITY_REORDER_TO_FRONT
-            | FLAG_ACTIVITY_SINGLE_TOP);
+    intent.addFlags(FLAG_ACTIVITY_REORDER_TO_FRONT | FLAG_ACTIVITY_SINGLE_TOP);
 
-        context.startActivity(intent);
-    }
-
+    context.startActivity(intent);
+  }
 }
