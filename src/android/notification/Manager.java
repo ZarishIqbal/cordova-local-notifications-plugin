720e31ef17d06f126d588b39feb20af612011e54
zarishiqbal <zarishiqbal.zi@gmail.com>
2023-10-16 17:00:00 +0300

    bug fixes

--- src/android/notification/Manager.java
+++ src/android/notification/Manager.java
@@ -34,6 +34,7 @@ import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.service.notification.StatusBarNotification;
